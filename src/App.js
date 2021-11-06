6eddd4d58f41c47906311e0e26c2dc68f0718a82
devdutt-pedalsup <devdutt@pedalsup.com>
2021-11-06 23:51:54 +0530

    after break
    6 Nov after diwali useContext,useReducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react'
 // const { ClassCounter } = require('./components/ClassCounter')
 // const { HookCounter } = require('./components/HookCounter')
 // const { PrevCounter } = require('./components/PrevCounter')
@@ -8,8 +9,35 @@ import './App.css';
 // const { EffectHookState } = require('./components/EffectHookState')
 // const { EffectHookMouse } = require('./components/EffectHookMouse')
 // const { HookFetch } = require('./components/HookFetch')
+// const { ComponentC } = require('./components/ComponentC')
+// export const UserContext = React.createContext();
+// export const ChannelContext = React.createContext();
+// const { Count } = require('./components/Count')
+// const { CountTwo } = require('./components/CountTwo')
+// const { A1 } = require('./components/A1')
+// const { B1 } = require('./components/B1')
+// const { C1 } = require('./components/C1')
+// export const CountContext = React.createContext();
+
+// const initialState = 0;
+// const reducer = (state ,action) => {
+  //   switch(action){
+    //     case 'increment':
+    //       return (state + 1);
+    //     case 'decrement':
+    //       return (state - 1);
+    //     case 'reset':
+    //       return (initialState);
+    //     default:
+    //       return state;
+    //   }
+    // }
+// }
+// const { DataOne } = require('./components/DataOne')
+// const { DataTwo } = require('./components/DataTwo')
 
 function App() {
+    // const [ count,dispatch ] = useReducer(reducer, initialState)
   return (
     <div className="App">
       {/* <ClassCounter /> */}
@@ -21,6 +49,21 @@ function App() {
       {/* <EffectHookState /> */}
       {/* <EffectHookMouse /> */}
       {/* <HookFetch /> */}
+      {/* <UserContext.Provider value={'devdutt'}>
+        <ChannelContext.Provider value={'CodeEvolution'}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider> */}
+      {/* <Count /> */}
+      {/* <CountTwo /> */}
+      {/* <CountContext.Provider value={{countState: count, countDispatch: dispatch}}>
+        Count - {count}
+        <A1 />
+        <B1 />
+        <C1 />
+      </CountContext.Provider> */}
+      {/* <DataOne /> */}
+      {/* <DataTwo /> */}
     </div>
   );
 }
